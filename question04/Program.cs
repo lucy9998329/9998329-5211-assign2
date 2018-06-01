ec55ebee5ad342162db167384f800039242f26e0
Luciany Laurentino <lucylaurentino@gmail.com>
2018-06-01 14:28:56 +1200

    Q4 using int.Parse to convert type string to int

--- question04/Program.cs
+++ question04/Program.cs
@@ -6,21 +6,12 @@ namespace question04
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Print out the reverse timestables (highest number first) for:");
-            
+
             int number = int.Parse(Console.ReadLine());
-            
 
-            for(int i = 13; i > 0; i--) 
-            {
-
-                if(i < 11) 
-                {
-                    Console.Write("0");
-                }
-                
-                int a = i - 1;
+            for(int i = 0; i < 12; i++) {
+                int a = i + 1;
                 Console.WriteLine($"{a} x {number} = {a * number}");
             }
         }
