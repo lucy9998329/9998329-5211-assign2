7c3ca7f8d251bcd7f622991f4c5a57cbe2b0b3e4
Luciany Laurentino <lucylaurentino@gmail.com>
2018-06-01 17:23:38 +1200

    Q5 added if statement to format the number to two digits

--- question05/Program.cs
+++ question05/Program.cs
@@ -11,7 +11,15 @@ namespace question05
             double number = double.Parse(Console.ReadLine());
 
             for(double i = 0; i < 12; i++) {
+
+                if(i < 9)
+
+                {
+                    Console.Write("0");
+                }
+                
                 double a = i + 1;
+                
                 Console.WriteLine($"{a} / {number} = {a / number}");
             }
         }
