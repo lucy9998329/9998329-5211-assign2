a21ecb00342803978d1242b3065a1706e38c24d4
Luciany Laurentino <lucylaurentino@gmail.com>
2018-06-01 16:58:26 +1200

    Q5 changing int with double to get results in decimals

--- question05/Program.cs
+++ question05/Program.cs
@@ -8,11 +8,11 @@ namespace question05
         {
             Console.WriteLine("Print out the division table for:");
 
-            int number = int.Parse(Console.ReadLine());
+            double number = double.Parse(Console.ReadLine());
 
-            for(int i = 0; i < 12; i++) {
-                int a = i + 1;
-                Console.WriteLine($"{a} x {number} = {a / number}");
+            for(double i = 0; i < 12; i++) {
+                double a = i + 1;
+                Console.WriteLine($"{a} / {number} = {a / number}");
             }
         }
     }
