63c5be9c9ea4d0d357a8bf8d30e48b4c6f6eddad
Luciany Laurentino <lucylaurentino@gmail.com>
2018-06-01 16:49:04 +1200

    Q5 using int.Parse to convert type string to int

--- question05/Program.cs
+++ question05/Program.cs
@@ -8,7 +8,7 @@ namespace question05
         {
             Console.WriteLine("Print out the division table for:");
 
-            int number = Console.ReadLine();
+            int number = int.Parse(Console.ReadLine());
 
             for(int i = 0; i < 12; i++) {
                 int a = i + 1;
